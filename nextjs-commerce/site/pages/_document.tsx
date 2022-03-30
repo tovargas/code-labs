f9208b231d882958c3d932bac5f1ca67f31c374c
Antonio Vargas <antonio.vargas@outlook.pt>
2022-03-30 15:58:43 +0100

    feat: Create NextJs Blog and Commerce Apps
    - Add navigation between pages
    - Integrate NewRelic Browser Agent
    - Add New Relic playground page
    - Send an Hello World Page Action to New Relic
    - Create dynamic pages
    - Use CSS modules
    - Create API route sample
    - Add Nest.JS Vercel Commerce
    - Add NewRelic Brwoser to the Commerce App

---
+++ nextjs-commerce/site/pages/_document.tsx
@@ -0,0 +1,19 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script async type="text/javascript" src="/js/newrelic.js" />
+        </Head>
+        <body className="loading">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
